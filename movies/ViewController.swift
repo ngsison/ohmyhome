b3228d45fee390185286c3c61107237bb684aa87
Nathaniel Brion Sison <nathaniel.sison@mynt.xyz>
2019-12-31 14:56:04 +0800

    Initial Commit

---
+++ movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Nathaniel Brion Sison on 31/12/2019.
+//  Copyright © 2019 Nathaniel Brion Sison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
